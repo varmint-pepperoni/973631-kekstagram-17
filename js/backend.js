d617e6a694be8c10ed3191112d10a02ee9ad2093
Irina Vakhrusheva <mrs.vakhrusheva@gmail.com>
2019-07-30 19:37:06 +0300

    Добавила валидацию хэш-тегов.

--- js/backend.js
+++ js/backend.js
@@ -7,8 +7,8 @@
     load: function (onLoad, onError) {
       window.load(SERVER_URL + '/data', null, 'GET', onLoad, onError);
     },
-    save: function () {
-      // На будущее
+    send: function (data, onLoad, onError) {
+      window.load(SERVER_URL, data, 'POST', onLoad, onError);
     }
   };
 })();
--- js/data.js
+++ js/data.js
@@ -196,16 +196,7 @@
   };
 
   var ajaxErrorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
+    window.error(errorMessage);
   };
 
   var photosContainerClickHandler = function (e) {
